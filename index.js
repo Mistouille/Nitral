3e46c0f094d9992c54ee5d398de9752bf6f36e9b
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-04-22 19:32:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -267,7 +267,41 @@ if (message.content === prefix + 'cat') {
     if (message.content ==="Team Wolf") {
         message.channel.send(":online-1: :wolf:**__TEAM WOLF EN FORCE__**:wolf:");
     }
-	
+	if (message.content.startsWith(prefix + "rps")){
+       if (!client.lockit) client.lockit = [];
+  const time = args.join(' ');
+  const validUnlocks = ['release', 'unlock'];
+  if (!time) return message.reply('Vous devez définir une durée pour le lockdown. En heures, minutes ou secondes');
+
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.send('Lockdown levé');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.send(`Channel lock pendant ${ms(ms(time), { long:true })}`).then(msg => {msg.delete(ms(time))}).then(() => {
+
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.send('Lockdown levé')).then(msg => {msg.delete(10000)});
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+
+      }).catch(error => {
+        console.log(error);
+      });
+    });
+  }
+};
        if (message.content === prefix +  "distrub") {
 	     if(message.author.id == "236627494764150784"){
     	message.channel.bulkDelete (1)
