f2bc282f4d3c31ae7a408c8c2d00d2186d937fe9
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-04-23 14:53:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -333,9 +333,10 @@ if (message.content.startsWith(prefix + "rps")){
       if (computer_choice == user_choice) {
         message.channel.send("Egalité!");
       }
-      else if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
-        message.channel.send ( computer_choice + " J'ai gagné!");
-      } else {
+       if (computer_choice < user_choice || computer_choice == 0 && user_choice == 2) {
+        message.channel.send (  " J'ai gagné!");
+      } 
+	  if (computer_choice > user_choice || computer_choice == 2 && user_choice == 0) {
         message.channel.send ("Tu as gagné!");
       }
     } 
