ebccdc03695b7ddef85203499beee629aca36294
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-04-25 12:12:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -800,65 +800,12 @@ if (message.content === prefix + "onmain") {
   	   	 .addField ("ID de l'utilisateur", message.author.id,false )
          .setTimestamp()
          .setFooter(`User Info`);
-  	  	 console.log(`${message.author.username} |UserInfo`)
+  	  	 
     message.channel.send(userembed)
-};
-if (message.content.startsWith === prefix +`userinfot`){
-let user = message.mentions.users.first();
-    if (!user) {
-        return message.reply('You must mention someone!');
-    }
-    const mentioneduser = message.mentions.users.first();
-    const joineddiscord = (mentioneduser.createdAt.getDate() + 1) + '-' + (mentioneduser.createdAt.getMonth() + 1) + '-' + mentioneduser.createdAt.getFullYear() + ' | ' + mentioneduser.createdAt.getHours() + ':' + mentioneduser.createdAt.getMinutes() + ':' + mentioneduser.createdAt.getSeconds();
-    message.delete();
-    let game;
-    if (user.presence.game === null) {
-        game = 'Not currently Playing.';
-    } else {
-        game = user.presence.game.name;
-    }
-    let messag;
-    if (user.lastMessage === null) {
-        messag = 'He didnt sent a message.';
-    } else {
-        messag = user.lastMessage;
-    }
-    let status;
-    if (user.presence.status === 'online') {
-        status = ':green_heart:';
-    } else if (user.presence.status === 'dnd') {
-        status = ':heart:';
-    } else if (user.presence.status === 'idle') {
-        status = ':yellow_heart:';
-    } else if (user.presence.status === 'offline') {
-        status = ':black_heart:';
-    }
-  // Let afk;
-  // if (user.presence.data.afk === true) {
-  //   afk = "✅"
-  // } else {
-  //   afk = "❌"
-  // }
-    let stat;
-    if (user.presence.status === 'offline') {
-        stat = 0x000000;
-    } else if (user.presence.status === 'online') {
-        stat = 0x00AA4C;
-    } else if (user.presence.status === 'dnd') {
-        stat = 0x9C0000;
-    } else if (user.presence.status === 'idle') {
-        stat = 0xF7C035;
-    }
-    const embed = new Discord.RichEmbed()
-  .addField('**UserInfo:**', `**name:** ${user.username}#${user.discriminator}\n**JoinedDiscord:** ${joineddiscord}\n**LastMessage:** ${messag}\n**Playing:** ${game}\n**Status:** ${status}\n**Bot?** ${user.bot}`, true)
-  .setThumbnail(user.displayAvatarURL)
-  .addField(`Roles:`, '``' + message.mentions.members.first().roles.map(r => r.name).join(', ') + '``')
-  .addField('DiscordInfo:', `**Discriminator:** ${user.discriminator}\n**ID:** ${user.id}\n**Username:** ${user.username}`)
-  .setAuthor(`Info for ${user.username} Below`, user.displayAvatarURL)
-  .setColor(stat);
-    message.channel.send(embed)
-  .catch(e => logger.error(e));
-};      //Kick
+	     console.log(`${message.author.username} |UserInfo`)
+
+};      
+	//Kick
       if (message.content.startsWith(prefix + "kick")) {
 
         if (!message.channel.permissionsFor(message.author).hasPermission("KICK_MEMBERS")) {
