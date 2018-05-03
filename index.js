753293bb2c886950187f691c568a979095c816ec
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-03 07:20:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,55 +46,7 @@ bot.on('ready',function() {
 bot.on("message", async message => {
 	const used = process.memoryUsage().heapUsed / 1024 / 1024;
 let defineduser = message.mentions.users.first();
-	let ms = bot.uptime;
-    let cd = 24 * 60 * 60 * 1000; // Calc days
-    let ch = 60 * 60 * 1000; // Calc hours
-    let cm = 60 * 1000; // Calc minutes
-    let cs = 1000; // Calc seconds
-    let days = Math.floor(ms / cd);
-    let dms = days * cd; // Days, in ms
-    let hours = Math.floor((ms - dms) / ch);
-    let hms = hours * ch; // Hours, in ms
-    let minutes = Math.floor((ms - dms - hms) / cm);
-    let mms = minutes * cm; // Minutes, in ms
-    let seconds = Math.round((ms - dms - hms - mms) / cs);
-    if (seconds === 60) {
-        minutes++; // Increase by 1
-        seconds = 0;
-    }
-    if (minutes === 60) {
-        hours++; // Inc by 1
-        minutes = 0;
-    }
-    if (hours === 24) {
-        days++; // Increase by 1
-        hours = 0;
-    }
-    let dateStrings = [];
-
-    if (days === 1) {
-        dateStrings.push('**1** jour');
-    } else if (days > 1) {
-        dateStrings.push('**' + String(days) + '** jours');
-    }
-
-    if (hours === 1) {
-        dateStrings.push('**1** heure' );
-    } else if (hours > 1) {
-        dateStrings.push('**' + String(hours) + '** heures');
-    }
-
-    if (minutes === 1) {
-        dateStrings.push('**1** minute');
-    } else if (minutes > 1) {
-        dateStrings.push('**' + String(minutes) + '** minutes');
-    }
-
-    if (seconds === 1) {
-        dateStrings.push('**1** seconde');
-    } else if (seconds > 1) {
-        dateStrings.push('**' + String(seconds) + '** secondes');
-    }
+	
 
     let dateString = '';
     for (let i = 0; i < dateStrings.length - 1; i++) {
@@ -476,7 +428,68 @@ if (!args[0]) {return message.channel.send (`Veuillez mettreun nom au role .`)
   message.channel.send("🏅 | **Tu as obtenu le nombre** " + result);
 }
 	if (message.content === prefix +  "uptime") {
-	    
+	    let ms = bot.uptime;
+    let cd = 24 * 60 * 60 * 1000; // Calc days
+    let ch = 60 * 60 * 1000; // Calc hours
+    let cm = 60 * 1000; // Calc minutes
+    let cs = 1000; // Calc seconds
+    let days = Math.floor(ms / cd);
+    let dms = days * cd; // Days, in ms
+    let hours = Math.floor((ms - dms) / ch);
+    let hms = hours * ch; // Hours, in ms
+    let minutes = Math.floor((ms - dms - hms) / cm);
+    let mms = minutes * cm; // Minutes, in ms
+    let seconds = Math.round((ms - dms - hms - mms) / cs);
+    if (seconds === 60) {
+        minutes++; // Increase by 1
+        seconds = 0;
+    }
+    if (minutes === 60) {
+        hours++; // Inc by 1
+        minutes = 0;
+    }
+    if (hours === 24) {
+        days++; // Increase by 1
+        hours = 0;
+    }
+    let dateStrings = [];
+
+    if (days === 1) {
+        dateStrings.push('**1** jour');
+    } else if (days > 1) {
+        dateStrings.push('**' + String(days) + '** jours');
+    }
+
+    if (hours === 1) {
+        dateStrings.push('**1** heure' );
+    } else if (hours > 1) {
+        dateStrings.push('**' + String(hours) + '** heures');
+    }
+
+    if (minutes === 1) {
+        dateStrings.push('**1** minute');
+    } else if (minutes > 1) {
+        dateStrings.push('**' + String(minutes) + '** minutes');
+    }
+
+    if (seconds === 1) {
+        dateStrings.push('**1** seconde');
+    } else if (seconds > 1) {
+        dateStrings.push('**' + String(seconds) + '** secondes');
+    }
+
+    let dateString = '';
+    for (let i = 0; i < dateStrings.length - 1; i++) {
+        dateString += dateStrings[i];
+        dateString += ', ';
+    }
+    if (dateStrings.length >= 2) {
+        dateString = dateString.slice(0, dateString.length - 2) + dateString.slice(dateString.length - 1);
+        dateString += 'et ';
+    }
+	
+    dateString += dateStrings[dateStrings.length - 1];
+	
     const embed = new Discord.RichEmbed()
   .setTimestamp()
   .setThumbnail(message.author.iconURL)
