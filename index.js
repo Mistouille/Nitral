ea24496e287bb1903cd95f6de62e618a6d86d85c
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2018-05-06 11:41:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1124,7 +1124,7 @@ if (message.content === prefix + "onmain") {
   	  //8ball
   function doMagic8BallVoodoo() {
  	var text = message.content.substring(7);
-var rand = ['Oui ','Assurément','Pas du tout ',"Demande à quelqu'un d'autre. " , "C'est sûre", 'Non', 'Je ne peux pas le dire maintenant ', ' Probablement que non ', ' Probablement que oui ' , 'Peut-être ', ' Jamais ', "Je n'est pas la réponse à " + text, "Je crois", "Sûrement \n\n\n \n \n pas" ];
+var rand = ['C\'est certain','Sans aucun doute','Oui définitivement', 'Les perspectives sont bonnes','Les signes indique très probablement ','Les signes indique très peu probable ', "Je n'est compris, essayez à nouveau",'Demander à nouveau plus tard', "Ne compte pas sur lui",'Ma réponse est non','Mes sources disent non', 'Les perspectives ne sont pas si bonnes', 'Très douteux', 'Oui ','Assurément','Pas du tout ',"Demande à quelqu'un d'autre. " , "C'est sûre", 'Non', 'Je ne peux pas le dire maintenant ', ' Probablement que non ', ' Probablement que oui ' , 'Peut-être ', ' Jamais ', "Je n'est pas la réponse à " + text, "Je crois", "Sûrement \n\n\n \n \n pas" ];
 
       return rand[Math.floor(Math.random()*rand.length)];
     }
